fb7a034115b116c768b98113c99ad6c135346a18
windy46 <842860890@qq.com>
2021-12-15 23:39:15 +0800

    update insertSort

--- src/main/java/com/dettol/utils/TestUtils.java
+++ src/main/java/com/dettol/utils/TestUtils.java
@@ -10,7 +10,7 @@ public class TestUtils {
         //insert(array); //OPEN FOR #insert
         //insert2(array2); //OPEN FOR #insert2
 //        printOut(array2); //OPEN FOR #insert2
-        insert8(array3);
+        insert9(array3);
         printOut(array3); //OPEN FOR #insert3
     }
 
@@ -160,6 +160,21 @@ public class TestUtils {
             }
             array[j] = value;
             i++;
+        }
+    }
+
+    public static void insert9(int[] array){
+        if(array == null || array.length == 0){
+            return;
+        }
+        for (int i = 1; i < array.length; i++) {
+             printOut(array);
+            for (int j = i; j > 0 && array[j - 1] > array[j]; j--){
+                int value = array[j];
+                array[j] = array[j - 1];
+                array[j - 1] = value;
+            }
+
         }
     }
 
