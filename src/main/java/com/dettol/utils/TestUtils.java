a92bc753e609189ab23ef1ac0f88d4267b9f3440
windy46 <842860890@qq.com>
2022-01-28 22:09:42 +0800

    update insertSort

--- src/main/java/com/dettol/utils/TestUtils.java
+++ src/main/java/com/dettol/utils/TestUtils.java
@@ -10,7 +10,7 @@ public class TestUtils {
         //insert(array); //OPEN FOR #insert
         //insert2(array2); //OPEN FOR #insert2
 //        printOut(array2); //OPEN FOR #insert2
-        insert52(array3);
+        insert53(array3);
         //printOut(array3); //OPEN FOR #insert3
     }
 
@@ -954,6 +954,20 @@ public class TestUtils {
             }
             array[j] = value;
             printOut(array);
+        }
+    }
+
+    public static void insert53(int[] array){
+        if(array == null){
+            return;
+        }
+        for (int i = 0; i < array.length ; i++) {
+            for (int j = i; j > 0 && array[j - 1] > array[j] ; j--) {
+                int value = array[j];
+                array[j] = array[j - 1];
+                array[j - 1] = value;
+            }
+            printOut(array); //cpmpleted
         }
     }
 
